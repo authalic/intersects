1d2cb1823e7bb72b667dce7eda25b737ee56ad89
Justin Johnson <authalic@gmail.com>
2020-05-13 10:43:57 -0600

    commiting final version of intersect_features

--- intersect_features.py
+++ intersect_features.py
@@ -1,11 +1,26 @@
-# Justin Johnson, May 2020
+# Justin Johnson
+# Senior GIS Analyst
+# Utah Division of Forestry, Fire and State Lands
+# May 2020
+
+'''
+Summary:
+This script runs through all feature layers in a geodatabase
+Feature layers are parcel boundary layers for each of the 29 counties in Utah
+There is also a polygon layer, created from a 30m raster, showing forested areas
+Forest cover data comes from the NLCD 2016 dataset
+(National Land Cover Dataset)
+For each county parcel layer, select the parcels that are privately owned
+from those selected parcels, select the parcels that intersect any forested area
+export that selection as a new layer of privately owned forested parcels for each county
+'''
 
 import arcpy
 import datetime
 import time
 
 print("starting")
-t1 = time.time() # start the timer
+t1 = time.time() # start the timer to measure total runtime
 
 # workspace must be set prior to listing feature classes
 input_gdb = r'T:\Shared drives\DNR_FFSL\Forestry\GIS\Forest Stewardship\Parcels_Utah_2020.gdb'
@@ -19,21 +34,30 @@ fcs = arcpy.ListFeatureClasses(feature_type='polygon')  # returns a list of stri
 
 # iterate through the feature classes
 for fc in fcs:
-    if fc != forest:
+    if fc != forest:  # skip the forest cover layer
 
         print('\n', fc)
 
         # select the private parcels
         print("   selecting private parcels")
         starttime = time.time()  # start the stopwatch
-        private_parcels = arcpy.SelectLayerByAttribute_management(fc, "NEW_SELECTION", "LOWER(OWN_TYPE) = 'private'")
+        private_parcels = arcpy.SelectLayerByAttribute_management(
+            fc,
+            "NEW_SELECTION",
+            "LOWER(OWN_TYPE) = 'private'"
+            )
         secs = time.time() - starttime
         print("     done - elapsed time: ", str(datetime.timedelta(seconds=secs)))
 
         # select the private parcels that intersect the forested areas
         print("   selecting forested private")
         starttime = time.time()  # start the stopwatch
-        private_forested = arcpy.SelectLayerByLocation_management(private_parcels, "INTERSECT", selection_type="SUBSET_SELECTION", select_features=forest)
+        private_forested = arcpy.SelectLayerByLocation_management(
+            private_parcels,
+            "INTERSECT",
+            selection_type="SUBSET_SELECTION",
+            select_features=forest
+            )
         secs = time.time() - starttime
         print("     done - elapsed time: ", str(datetime.timedelta(seconds=secs)))
 
@@ -45,5 +69,5 @@ for fc in fcs:
         secs = time.time() - starttime
         print("     done - elapsed time: ", str(datetime.timedelta(seconds=secs)))
 
-t2 = time.time()
+t2 = time.time() # stop the total runtime timer
 print("done - elapsed time: ", str(datetime.timedelta(seconds=t2 - t1)))
